b9fa2729442222ed2125b67377939a0dd72e4698
Danielle McLean <dani@00dani.me>
2024-03-27 20:29:45 +1100

    style: attempt to pretty up Product component

--- packages/front-end/components/Product.vue
+++ packages/front-end/components/Product.vue
@@ -11,27 +11,43 @@
     <div class="mb-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
         <div class="border border-grey cursor-pointer rounded-lg hover:shadow w-full">
             <div class="px-4 pt-6">
-                <div class="h-64">
-                    <div class="h-full bg-center bg-contain bg-no-repeat" :style="`background-image: url(https://cataas.com/cat?c=${product.id})`"></div>
+                <div class="inline-block grid justify-end">
+                    <div class="text-red-600">⏱ 10 hrs</div>
                 </div>
-                <div class="text-center text-blue font-bold text-lg px-4 mt-8 h-24 line-clamp-2">
-                    {{ product.name }}
-                </div>
-                <div class="flex pb-2">
-                    <div class="w-1/2"></div>
-                    <div class="text-xl font-black w-1/2">
-                        <div class="inline-block grid justify-end">
-                            <div class="text-sm">
-                                from
-                            </div> 
-                            <div class="text-red text-3xl font-bold">
-                                ${{ product.price }}
-                            </div> 
-                            <div class="text-xs">
-                                <div> / carton</div>
-                            </div>
-                        </div>
+
+                <div class="h-58 flex items-center">
+                    <img class="w-1/2 mr-2" :src="`https://cataas.com/cat?c=${product.id}`" />
+                    <div class="font-bold text-lg text-center line-clamp-2">
+                        {{ product.name }}
                     </div>
+                </div>
+
+                <div class="text-sm my-4">
+                    from
+                    <span class="text-red-600">
+                        <span class="text-3xl font-bold">${{ product.price }}</span> 
+                        / carton
+                    </span>
+                </div>
+
+                <div class="text-sm mb-2 flex justify-between">
+                    <div>BBD</div>
+                    <div class="font-bold">[DD/MM/YYYY]</div>
+                </div>
+
+                <div class="text-sm mb-2 flex justify-between">
+                    <div>Qty Available</div>
+                    <div>[xx] cartons</div>
+                </div>
+
+                <div class="text-sm mb-2 flex justify-between">
+                    <div>Min Order</div>
+                    <div>[xx] cartons</div>
+                </div>
+
+                <div class="text-sm mb-2 flex justify-between">
+                    <div>State</div>
+                    <div>[State]</div>
                 </div>
             </div>
         </div>
